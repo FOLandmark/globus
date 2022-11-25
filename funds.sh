84ddd51df864d9865a49799ea610be8b88c6096b
FOLandmark <118616163+FOLandmark@users.noreply.github.com>
2022-11-24 20:35:12 -0700

    Update funds.sh

--- funds.sh
+++ funds.sh
@@ -4,12 +4,8 @@ echo this file computes the funds available for the globus project
 echo State the total project budget for globus
 sleep 1
 read budget
-
-
 echo The total budget for globus is $budget
 echo What will each team member be paid annually?
- 
 read salary
-
 let sales=$(($budget/$salary))
 echo "The average pay for members of project globus is" $sales
